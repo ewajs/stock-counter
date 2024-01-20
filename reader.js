b63a8d81c0fa4f60bdf6dfdcba64d26d255b8660
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2024-01-20 18:51:32 -0300

    Add/Edit/Remove

--- reader.js
+++ reader.js
@@ -78,24 +78,29 @@ function addBarcode(barcode) {
     barcode = barcode.trim();
     if (barcode === '') return;
     // Add to list, table and reset
-    sections[currentSection].push(barcode);
-    addBarcodeToTable(barcode, sections[currentSection].length - 1);
+    const index = sections[currentSection].length;
+    const barcodeData = {barcode, index, amount: 1};
+    sections[currentSection].push(barcodeData);
+    addBarcodeToTable(barcodeData);
     barcodeInput.value = '';
     updateItemsInSection();
     updateTotalItems();
 }
 
-function addBarcodeToTable(barcode, idx) {
+function addBarcodeToTable(barcodeData) {
     const tr = document.createElement('tr');
-    tr.dataset.barcode = barcode;
-    tr.dataset.index = idx;
+    tr.dataset.barcode = barcodeData.barcode;
+    tr.dataset.index = barcodeData.index;
+    tr.dataset.amount = barcodeData.amount;
     tr.classList.add('barcode');
     const td1 = document.createElement('td');
     td1.innerHTML = '<i class="bi bi-pencil"></i>';
     const td2 = document.createElement('td');
+    td2.classList.add('amount');
     td2.innerText = 1;
     const td3 = document.createElement('td');
-    td3.innerText = barcode;
+    td3.classList.add('barcode');
+    td3.innerText = barcodeData.barcode;
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
@@ -111,3 +116,59 @@ barcodeInput.addEventListener('keyup', (e) => {
 
 barcodeEnter.addEventListener('click', () => addBarcode(barcodeInput.value));
 
+function deleteItem(index) {
+    const indexToRemove = sections[currentSection].findIndex(el => el.index === index);
+    sections[currentSection].splice(indexToRemove, 1);
+    reindexSection();
+}
+
+function reindexSection() {
+    sections[currentSection] = sections[currentSection].map((item, index) => {
+        return {...item, index};
+    });
+    updateSections();
+    updateTotalItems();
+}
+
+tableBody.addEventListener('click', (e) => {
+    // Only listen for clicks on the edit button
+    if (!e.target.classList.contains('bi-pencil')) return;
+    const targetBarcodeRow = e.target.closest('tr');
+    const index = targetBarcodeRow.dataset.index;
+    Swal.fire({
+        title: `Editar ${targetBarcodeRow.dataset.barcode}`,
+        html: `
+        <div class="swal-edit">
+            <p>Código de Barras/Cantidad</p>
+            <div class="input-group">
+                <input type="text" value="${targetBarcodeRow.dataset.barcode}" class="form-control barcode"/>
+                <input type="number" value="${targetBarcodeRow.dataset.amount}" class="form-control amount"/>
+            </div>
+        </div>
+        `,
+        focusConfirm: false,
+        showDenyButton: true,
+        showCancelButton: true,
+        denyButtonText: 'Borrar',
+        cancelButtonText: 'Cancelar',
+        preConfirm: () => {
+          return [
+            document.querySelector('.swal-edit .barcode').value,
+            document.querySelector('.swal-edit .amount').value
+          ];
+        }
+      }).then((result) => {
+            if (result.isDismissed) return;
+            else if (result.isDenied) deleteItem(index);
+            else if (result.isConfirmed) {
+                const [barcode, amount] = result.value;
+                console.log("Updating", barcode, amount)
+                targetBarcodeRow.dataset.barcode = barcode;
+                targetBarcodeRow.dataset.amount = amount;
+                const indexToEdit = sections[currentSection].findIndex(el => el.index === index);
+                sections[currentSection][indexToEdit] = {index, barcode, amount};
+                targetBarcodeRow.querySelector('.barcode').innerText = barcode;
+                targetBarcodeRow.querySelector('.amount').innerText = amount;
+            }
+      });
+});
