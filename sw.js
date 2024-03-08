abd1b6a39e2587d75863f560f1b1128755952ef2
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2024-03-07 21:20:31 -0300

    remove root from sw

--- sw.js
+++ sw.js
@@ -2,17 +2,17 @@ self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open("v1").then(function (cache) {
       return cache.addAll([
-        "stock-counter/",
-        "stock-counter/index.html",
-        "stock-counter/styles/styles.css",
-        "stock-counter/styles/bootstrap.min.css",
-        "stock-counter/styles/bootstrap-icons.min.css",
-        "stock-counter/styles/fonts/bootstrap-icons.woff2",
-        "stock-counter/js/reader.js",
-        "stock-counter/js/bootstrap.bundle.min.js",
-        "stock-counter/js/sweetalert2.min.js",
-        "stock-counter/js/papaparse.min.js",
-        "stock-counter/icon/icon.png",
+        "/",
+        "/index.html",
+        "/styles/styles.css",
+        "/styles/bootstrap.min.css",
+        "/styles/bootstrap-icons.min.css",
+        "/styles/fonts/bootstrap-icons.woff2",
+        "/js/reader.js",
+        "/js/bootstrap.bundle.min.js",
+        "/js/sweetalert2.min.js",
+        "/js/papaparse.min.js",
+        "/icon/icon.png",
       ]);
     })
   );
