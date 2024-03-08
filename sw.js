e83573dd876806ac907258d2c4e720e741afa36c
Ezequiel Wajs <ezequiel.wajs@gmail.com>
2024-03-07 21:24:49 -0300

    add relative back

--- sw.js
+++ sw.js
@@ -2,17 +2,17 @@ self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open("v1").then(function (cache) {
       return cache.addAll([
-        "stock-counter/",
-        "stock-counter/index.html",
-        "stock-counter/styles/styles.css",
-        "stock-counter/styles/bootstrap.min.css",
-        "stock-counter/styles/bootstrap-icons.min.css",
-        "stock-counter/styles/fonts/bootstrap-icons.woff2",
-        "stock-counter/js/reader.js",
-        "stock-counter/js/bootstrap.bundle.min.js",
-        "stock-counter/js/sweetalert2.min.js",
-        "stock-counter/js/papaparse.min.js",
-        "stock-counter/icon/icon.png",
+        "../stock-counter/",
+        "../stock-counter/index.html",
+        "../stock-counter/styles/styles.css",
+        "../stock-counter/styles/bootstrap.min.css",
+        "../stock-counter/styles/bootstrap-icons.min.css",
+        "../stock-counter/styles/fonts/bootstrap-icons.woff2",
+        "../stock-counter/js/reader.js",
+        "../stock-counter/js/bootstrap.bundle.min.js",
+        "../stock-counter/js/sweetalert2.min.js",
+        "../stock-counter/js/papaparse.min.js",
+        "../stock-counter/icon/icon.png",
       ]);
     })
   );
